fbd7bf2529f1d9c2ab0a1c3c7f93ed4f3c0f5d03
Todd Gallimore <toddgallimore@gmail.com>
2022-09-15 16:36:09 +0100

    Endpoint to renew bank connection

--- functions/routes/bank-connections.js
+++ functions/routes/bank-connections.js
@@ -40,4 +40,43 @@ router.post('/truelayer-callback', async (req, res) => {
   }
 });
 
+router.post('/truelayer-renew-callback', async (req, res) => {
+  const { code, redirectUri, connectionId } = req.body;
+  const now = new Date();
+  const uid = req.user.uid;
+
+  try {
+    const db = getFirestore();
+    const connectionsCollection = db.collection('BankConnections');
+    const document = connectionsCollection.doc(connectionId);
+    const connection = await document.get();
+
+    const tokens = await exchangeCodeForToken(code, decodeURI(redirectUri));
+    const trueLayerAccounts = await getAccounts(tokens.access_token);
+    const newConnection = {
+      uid,
+      token: encrypt(tokens.access_token),
+      refreshToken: encrypt(tokens.refresh_token),
+      accounts: trueLayerAccounts.results.map(({account_id}) => {
+        const connectedAccount = connection.data().accounts.find(({account_id: accountId}) => account_id === accountId);
+        return {
+          account_id,
+          registered: connectedAccount?.registered || now.toISOString(),
+          authorised: now.toISOString(),
+          expires: addDays(now, 90).toISOString(),
+          primary: connectedAccount?.primary || false
+        }
+      }),
+      expires: addSeconds(now, tokens.expires_in)
+    };
+    
+    await document.set(newConnection);
+    return res.json({});
+  }
+  catch(e) {
+    res.status(400);
+    return res.json(e);
+  }
+});
+
 module.exports = router;
