4cf4b7b45a7c3bdb0fff1838cca737baab6e2d8a
Todd Gallimore <toddgallimore@gmail.com>
2022-10-09 23:17:17 +0100

    Syncing accounts after connecting

--- functions/routes/connections.js
+++ functions/routes/connections.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const addSeconds = require('date-fns/addSeconds');
 const addDays = require('date-fns/addDays');
 const { encrypt } = require('../util/secure');
-const { formatDate } = require('../util/date');
 
-const { exchangeCodeForToken, getAccounts, getAccountBalance, getTransactions } = require('../truelayer/api');
+const { exchangeCodeForToken, getAccounts, getAccountBalance } = require('../truelayer/api');
+const { syncBankConnection } = require('../util/sync');
 
 router.post('/connect', async (req, res) => {
   const { code, redirectUri } = req.body;
@@ -21,22 +21,21 @@ router.post('/connect', async (req, res) => {
     const accountsCollection = db.collection('Accounts');
     const tokensCollection = db.collection('Tokens');
     const promises = [];
-    trueLayerAccounts.forEach(async (account) => {
-      const balance = await getAccountBalance(account.account_id, tokens.access_token);
-
-      const accountPromise = accountsCollection
-        .doc(account.account_id)
+    for (let i = 0; i < trueLayerAccounts.length; i++) {
+      const account = trueLayerAccounts[i];
+      const accountDocument = accountsCollection.doc(account.account_id);
+      const accountPromise = accountDocument
         .set({
           uid,
           account_id: account.account_id,
           authorised: now,
           expires: addDays(now, 90),
           nickname: '',
-          connection_data: { account, balance },
+          connection_data: { account },
           budgets: [],
           balance_pots: [],
           overdraft_available: false
-      });
+        });
       promises.push(accountPromise);
 
       const tokenPromise = tokensCollection
@@ -48,11 +47,20 @@ router.post('/connect', async (req, res) => {
           refresh_token: encrypt(tokens.refresh_token),
           expires: addSeconds(now, tokens.expires_in)
         });
-        promises.push(tokenPromise);
-    });
+      promises.push(tokenPromise);
 
-    const results = Promise.all(promises);
-    return res.json(results);
+      await accountPromise;
+      const syncPromise = syncBankConnection({
+        uid,
+        account_id: account.account_id,
+        token: tokens.access_token,
+        accountDocument
+      });
+      promises.push(syncPromise)
+    }
+
+    await Promise.all(promises);
+    return res.json({});
   }
   catch(e) {
     res.status(400);
---
+++ functions/util/sync.js
@@ -0,0 +1,88 @@
+const { getFirestore } = require('firebase-admin/firestore');
+const express = require('express');
+const addDays = require('date-fns/addDays');
+const isBefore = require('date-fns/isBefore');
+
+const { getAccountBalance, getTransactions } = require('../truelayer/api');
+
+const syncBankConnection = async ({ uid, account_id, token, accountDocument }) => {
+  const now = new Date();
+
+  try {
+    const db = getFirestore();
+
+    const accountSnapshot = await accountDocument.get();
+    const account = accountSnapshot.data();
+    if (!account) {
+      return Promise.reject();
+    }
+
+    const halfYearAgo = addDays(now, -183);
+    const latestSync = account.latest_sync?.toDate();
+    const latest_sync = latestSync && !isBefore(latestSync, halfYearAgo)
+      ? latestSync
+      : halfYearAgo;
+
+
+    const balance = await getAccountBalance(account_id, token);
+    
+    const transactions = await getTransactions(account_id, latest_sync.toISOString(), now.toISOString(), token);
+
+    // 1. Update account balance and sync data
+    await accountDocument.set({
+      latest_sync: now,
+      connection_data: {
+        ...account.connection_data,
+        balance
+      }
+    }, { merge: true })
+
+    // 2. Sync transactions
+    const batch = db.batch();
+
+    for (let i = 0; i < transactions.length; i++) {
+      const connection_data = transactions[i];
+      const transacitonDocument = db.collection('BankTransactions').doc(connection_data.transaction_id);
+      const transaction = await transacitonDocument.get();
+
+      if (transaction.data()) {
+        batch.set(transacitonDocument, { connection_data }, { merge: true });
+      } else {
+        batch.set(transacitonDocument, {
+          connection_data,
+          uid,
+          account_id,
+          title: connection_data.description,
+          date: new Date(connection_data.timestamp),
+          amount: connection_data.amount * 100,
+          receipt: null,
+          categories: connection_data.transaction_classification?.[0]
+            ? [{
+              report_category: connection_data.transaction_classification[0],
+              budget_category: connection_data.transaction_classification[0],
+              allocation: connection_data.amount * 100,
+            }]
+            : null,
+          /**
+           * TODO
+           * 1. Find subscription and update
+           * 2. Check for auto saving rules on the account and apply
+           */
+          subscription: null,
+          auto_saving: null,
+          categories: null,
+        });
+      }
+    }
+
+    await batch.commit();
+    return Promise.resolve();
+
+  } catch(e) {
+    return Promise.reject(e);
+  }
+}
+
+module.exports = {
+  syncBankConnection
+};
